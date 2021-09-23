0e15fef12c7821458d1a00b32e2dd5ab4b40b76a
Suresh-Muthurakku <suresh.muthurakku@gmail.com>
2021-09-23 12:43:45 +0200

    typ change on service

--- favoriteservice/src/main/java/com/hcl/favourite/service/FavouriteAccountListService.java
+++ favoriteservice/src/main/java/com/hcl/favourite/service/FavouriteAccountListService.java
@@ -3,5 +3,5 @@ package com.hcl.favourite.service;
 import org.springframework.stereotype.Service;
 
 @Service
-public class FavoriteAccountListService {
+public class FavouriteAccountListService {
 }
