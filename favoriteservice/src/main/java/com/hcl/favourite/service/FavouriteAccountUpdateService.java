1f3f44e1c815ec5065f4f0bee7c2ccfb3ec55bbd
Suresh-Muthurakku <suresh.muthurakku@gmail.com>
2021-09-23 16:03:14 +0200

    iBan logic added

--- favoriteservice/src/main/java/com/hcl/favourite/service/FavouriteAccountUpdateService.java
+++ favoriteservice/src/main/java/com/hcl/favourite/service/FavouriteAccountUpdateService.java
@@ -3,18 +3,35 @@ package com.hcl.favourite.service;
 import com.hcl.favourite.domain.FavouriteAccount;
 import com.hcl.favourite.repository.FavouriteAccountRespository;
 import com.hcl.favourite.repository.UserRepository;
+import com.hcl.favourite.service.bankname.ResolveBankNameService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 public class FavouriteAccountUpdateService {
 
     @Autowired
     private FavouriteAccountRespository favouriteAccountRespository;
+    @Autowired
+    private ResolveBankNameService resolveBankNameService;
+    @Autowired
+    private IbanToBankCode ibanToBankCode;
 
 
     public FavouriteAccount updateFavouriteAccount(FavouriteAccount favouriteAccount){
-        return favouriteAccountRespository.save(favouriteAccount);
+        FavouriteAccount returnFavouriteAccount = favouriteAccountRespository.save(favouriteAccount);
+
+        try{
+            String iBan = favouriteAccount.getIban();
+            String bankCode = ibanToBankCode.resolve(iBan);
+            resolveBankNameService.submitResolveBankNameEvent(iBan, bankCode);
+
+        }catch(Exception e){
+            log.error("Iban not found", e);
+        }
+        return returnFavouriteAccount;
     }
 
 }
