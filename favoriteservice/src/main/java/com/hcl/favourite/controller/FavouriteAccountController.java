71e1bb69ad15fad0518cb28d1c72d06305b58ec7
Szymon Knitter <szknitter@wp.pl>
2021-09-23 15:42:18 +0200

    id:task4 Body for listeners

--- favoriteservice/src/main/java/com/hcl/favourite/controller/FavouriteAccountController.java
+++ favoriteservice/src/main/java/com/hcl/favourite/controller/FavouriteAccountController.java
@@ -39,8 +39,11 @@ public class FavouriteAccountController {
     private UserService userService;
 
     @GetMapping()
-    public ResponseEntity<Page<FavouriteAccount>> getFavorites(@RequestHeader("userId") Long userId,
+    public ResponseEntity<Page<FavouriteAccount>> getFavorites(@RequestHeader("User-Id") Long userId,
                                                                @PageableDefault(value=5) Pageable pageable) {
+        if (!userService.isValidUser(userId)) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
         Page<FavouriteAccount> page = favouriteAccountListService.viewFavorites(userId, pageable);
         return new ResponseEntity(page, HttpStatus.OK);
     }
